31a9219bf69484229fca2077c482270d6fd298b6
Salvador HM <salvadorhm@gmail.com>
2022-08-08 14:48:32 -0500

    FIX DELETE cliente

--- consultas/borrar_cliente.php
+++ consultas/borrar_cliente.php
@@ -2,15 +2,11 @@
     try {
         include("conexion.php");
         $id_cliente = $_POST["id_cliente"];
-        $nombre = $_POST["nombre"];
-        $email = $_POST['email'];
-        
+
         //echo "id_cliente " . $id_cliente;
-        //echo "nombre " . $nombre;
-        //echo "email " . $email;
         
-        $sentencia = $db->prepare("UPDATE clientes set nombre=?, email=? WHERE id_cliente=?");
-        $sentencia->execute([$nombre,$email,$id_cliente]);
+        $sentencia = $db->prepare("DELETE FROM clientes WHERE id_cliente= ?");
+        $sentencia->execute([$id_cliente]);
         $db = null;
         // Redirecciona al index
         header("Location: ../index.php");
