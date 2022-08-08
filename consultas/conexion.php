baa0199d55777e31f2e53fbf841896c5be6a6ef0
Salvador HM <salvadorhm@gmail.com>
2022-08-08 13:31:02 -0500

    PHP mysql

--- consultas/conexion.php
+++ consultas/conexion.php
@@ -1,16 +1,16 @@
 <?php 
     try {
-        $db_host = "127.0.0.1";
-        $db_username = "root";
-        $db_password = "toor";
-        $db_name = "papeleria";
+        $db_host = "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $db_username = "q01syfn6oy0g5d4z";
+        $db_password = "zb4u3perwq61gktu";
+        $db_name = "rzqqccli1jp4pp54";
         $db_port = "3306";
         $db_charset = "utf8";
         $db_collation = "utf8_unicode_ci";
         $db_engine = "InnoDB";
 
-        // $db = new PDO("mysql:host=$db_host;dbname=$db_name;port=$db_port;charset=$db_charset;collation=$db_collation", $db_username, $db_password);
-        $db = new PDO("sqlite:sql/clientes.sqlite");
+        $db = new PDO("mysql:host=$db_host;dbname=$db_name;port=$db_port;charset=$db_charset;collation=$db_collation", $db_username, $db_password);
+        // $db = new PDO("sqlite:sql/clientes.sqlite");
         
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
