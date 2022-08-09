4cce1af468ac0ab8a2a4568500ee8f5bd007d6dd
Salvador HM <salvadorhm@gmail.com>
2022-08-09 09:59:10 -0500

    NEW DB

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ apt install php7.4-mysql
 apt install php7.4-sqlite3
 
 https://code.tutsplus.com/es/tutorials/why-you-should-be-using-phps-pdo-for-database-access--net-12059
+
+PDO_DBLIB (FreeTDS/Microsoft SQL Server/Sybase)
+PDO_FIREBIRD (Firebird/Interbase 6)
+PDO_IBM (DB2 de IBM)
+PDO_INFORMIX (Servidor dinámico Informix de IBM)
+PDO_MYSQL (MySQL 3.x/4.x/5.x)
+PDO_OCI (Oracle Call Interface)
+PDO_ODBC (ODBC v3 (DB2 de IBM, unixODBC y ODBC para win32))
+PDO_PGSQL (PostgreSQL)
+PDO_SQLITE (SQLite 3 y SQLite 2)
+PDO_4D (4D)
--- consultas/conexion.php
+++ consultas/conexion.php
@@ -20,10 +20,10 @@
         $db=null;
 
         if($database=="MySQL"){
-            $db_host = "x71wqc4m22j8e3ql.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-            $db_username = "y1vq1dvswxlhzo8q";
-            $db_password = "neo6dzwzew336zfy";
-            $db_name = "ozyctasxrl9vksgn";
+            $db_host = "t07cxyau6qg7o5nz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+            $db_username = "xthomu8s6mv64jqs";
+            $db_password = "uxtixtel1iesadnb";
+            $db_name = "qmyw9tnk0yg5m5wx";
             $db_port = "3306";
             $db_charset = "utf8";
             $db_collation = "utf8_unicode_ci";
